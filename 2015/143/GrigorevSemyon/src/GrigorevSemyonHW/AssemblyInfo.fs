9c92f87b1de08e29e95d50d412cb9cd1f6766e3f
rsdpisuy <rsdpisuy@gmail.com>
2015-08-18 16:15:57 +0300

    Test folder.

---
+++ 2015/143/GrigorevSemyon/src/GrigorevSemyonHW/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("GrigorevSemyonHW")>]
+[<assembly: AssemblyProductAttribute("GrigorevSemyonHW")>]
+[<assembly: AssemblyDescriptionAttribute("my hw")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"

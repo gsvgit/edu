114e0aaf5c0f3c678f147a06c3eb49f3cf3fd709
SokolovEgor <egos1700@gmail.com>
2015-09-04 11:39:48 +0300

    1

---
+++ 2015/143/SokolovEgor/tests/SokolovEgorHW.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module SokolovEgorHW.Tests
+
+open SokolovEgorHW
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)

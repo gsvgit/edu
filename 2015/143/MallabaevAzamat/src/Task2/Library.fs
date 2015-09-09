add23f1c0dfa2597a5f6e9daec7bf9e67eae8311
Mallabaev Azamat (Fenex9428) <home9428@yandex.ru, amallabaev@yandex.ru>
2015-09-09 22:03:37 +0300

    Added Task2 solution
    Решение x^4 + x^3 + x^2 + x влоб

---
+++ 2015/143/MallabaevAzamat/src/Task2/Library.fs
@@ -0,0 +1,24 @@
+﻿namespace Task2
+
+/// Documentation for my library
+///
+/// ## Example
+///
+///     let h = main 1
+///     printfn "%d" h
+///
+module Library = 
+  
+  /// Returns a^n
+  /// 
+  /// ## Parameters
+  /// - `a` - base
+  /// - `n` - exponent
+  let rec (^) (a:int) (n:int) = if n < 0 then 1 else if n = 0 then 1 else a * (a ^ (n - 1))
+
+  /// Returns x^4 + x^3 + x^2 + x
+  ///
+  /// ## Parameters
+  ///  - `x` - whatever
+  let main x = (x ^ 4) + (x ^ 3) + (x ^ 2) + x
+

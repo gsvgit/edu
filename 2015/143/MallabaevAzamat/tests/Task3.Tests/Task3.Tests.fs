6181e238fd1ac0a3b5d5797d35cf572c52dd4dbe
Mallabaev Azamat (Fenex9428) <home9428@yandex.ru, amallabaev@yandex.ru>
2015-09-10 01:00:23 +0300

    Renamed tests

--- 2015/143/MallabaevAzamat/tests/Task3.Tests/Task3.Tests.fs
+++ 2015/143/MallabaevAzamat/tests/Task3.Tests/Task3.Tests.fs
@@ -4,19 +4,19 @@ open Task3
 open NUnit.Framework
 
 [<Test>]
-let ``main 0 returns 0`` () =
+let ``Task3:main 0 returns 0`` () =
   let result = Library.main 0
   printfn "%i" result
   Assert.AreEqual(0,result)
 
 [<Test>]
-let ``main 1 returns 4`` () =
+let ``Task3:main 1 returns 4`` () =
   let result = Library.main 1
   printfn "%i" result
   Assert.AreEqual(4,result)
   
 [<Test>]
-let ``main 10 returns 11110`` () =
+let ``Task3:main 10 returns 11110`` () =
   let result = Library.main 10
   printfn "%i" result
   Assert.AreEqual(11110,result)

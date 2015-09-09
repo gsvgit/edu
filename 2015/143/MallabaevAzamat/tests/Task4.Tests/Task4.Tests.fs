7b8b3993e19c09fb80cdfe6aefd5010227759511
Mallabaev Azamat (Fenex9428) <home9428@yandex.ru, amallabaev@yandex.ru>
2015-09-10 00:58:32 +0300

    Added Task4 solution
    Выбор элементов множества, не больших данного

---
+++ 2015/143/MallabaevAzamat/tests/Task4.Tests/Task4.Tests.fs
@@ -0,0 +1,22 @@
+﻿module Task4.Tests
+
+open Task4
+open NUnit.Framework
+
+[<Test>]
+let ``Task4:main [|12; 25; 17; 46|] 19 returns [|12; 17|]`` () =
+  let result = Library.main [|12; 25; 17; 46|] 19
+  printfn "%A" result
+  Assert.AreEqual([|12; 17|], result)
+
+[<Test>]
+let ``Task4:main [||] 9 returns [||]`` () =
+  let result = Library.main [||] 9
+  printfn "%A" result
+  Assert.AreEqual([||], result)
+
+[<Test>]
+let ``Task4:main [|12; 25; 17; 46|] 98 returns [|12; 25; 17; 46|]`` () =
+  let result = Library.main [|12; 25; 17; 46|] 98
+  printfn "%A" result
+  Assert.AreEqual([|12; 25; 17; 46|], result)

b9f543cad7f698895a8212198f76840fd99e4f14
Mallabaev Azamat <home9428@yandex.ru>
2015-09-14 01:38:35 +0300

    Remove bug
    Properties convert to functions

--- 2015/143/MallabaevAzamat/src/Task5/Task5.fs
+++ 2015/143/MallabaevAzamat/src/Task5/Task5.fs
@@ -1,4 +1,4 @@
 ﻿module Task5
 
 let main inArray lowBound highBound =
-    Array.map (fun (i, x) -> i) (Array.filter (fun (i, x) -> x < lowBound && x > highBound) (Array.mapi (fun i x -> (i, x)) inArray))
+    Array.map (fun (i, x) -> i) (Array.filter (fun (i, x) -> x < lowBound || x > highBound) (Array.mapi (fun i x -> (i, x)) inArray))
--- 2015/143/MallabaevAzamat/tests/Task5.Tests/Task5.Tests.fs
+++ 2015/143/MallabaevAzamat/tests/Task5.Tests/Task5.Tests.fs
@@ -4,19 +4,19 @@ open NUnit.Framework
 open Task5
 
 [<Test>]
-let ``main [||] 19 110 returns [||]`` =
+let ``main [||] 19 110 returns [||]``() =
     let result = main [||] 19 110 
     printfn "%A" result
     Assert.AreEqual([||], result)
     
 [<Test>]
-let ``main [|15; 272; 2; 18; 89; 1111; 6545353|] 19 1110 returns [|1; 3; 4; 6; 7|]`` =
+let ``main [|15; 272; 2; 18; 89; 1111; 6545353|] 19 1110 returns [|0; 2; 3; 5; 6|]``() =
     let result = main [|15; 272; 2; 18; 89; 1111; 6545353|] 19 1110 
     printfn "%A" result
-    Assert.AreEqual([|1; 3; 4; 6; 7|], result)
+    Assert.AreEqual([|0; 2; 3; 5; 6|], result)
     
 [<Test>]
-let ``main [|100|] 19 110 returns [||]`` =
+let ``main [|100|] 19 110 returns [||]``() =
     let result = main [|100|] 19 110
     printfn "%A" result
     Assert.AreEqual([||], result)

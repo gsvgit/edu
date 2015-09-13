6c0d47cb10facec436b5f6177f1d2f5086730ca4
SokolovEgor <egos1700@gmail.com>
2015-09-14 00:47:30 +0300

    4, 6, 7

---
+++ 2015/143/SokolovEgor/src/Task4/Task4.fs
@@ -0,0 +1,10 @@
+﻿module Task4
+
+let f_main (inArray:array<int>) (num:int) = 
+    for i in 0..inArray.Length-1 do 
+        if inArray.[i] <= num then inArray.[i] <- i else inArray.[i] <- (num+1)
+    inArray
+
+let main (inArray:array<int>) (num:int) = 
+    Array.filter (fun x -> x <> (num+1)) (f_main inArray num)
+

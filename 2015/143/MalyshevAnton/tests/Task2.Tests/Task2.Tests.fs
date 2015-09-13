c05657db8868972ef33b2fb048160136f8811c00
MalyshevAnton <daeron.rus@gmail.com>
2015-09-14 01:16:22 +0300

    Task2 and Task3

---
+++ 2015/143/MalyshevAnton/tests/Task2.Tests/Task2.Tests.fs
@@ -0,0 +1,22 @@
+﻿module Task2.Tests
+
+open Task2
+open NUnit.Framework
+
+[<Test>]
+let ``Task2:main 0 returns 1`` () =
+  let res = Task2.main 0
+  printfn "%i" res
+  Assert.AreEqual(0,res)
+
+[<Test>]
+let ``Task2:main 1 returns 5`` () =
+  let res = Task2.main 1
+  printfn "%i" res
+  Assert.AreEqual(5,res)
+  
+[<Test>]
+let ``Task2:main -2 returns 11`` () =
+  let res = Task2.main -2
+  printfn "%i" res
+  Assert.AreEqual(11,res) 

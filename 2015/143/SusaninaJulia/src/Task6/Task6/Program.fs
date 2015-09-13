211126d36e3e572e8ad1d8eaee0e6ee656e34544
SusaninaJulia <u.a.susxv@mail.ru>
2015-09-13 22:12:03 +0300

    2,3,4,5,6,7

---
+++ 2015/143/SusaninaJulia/src/Task6/Task6/Program.fs
@@ -0,0 +1,9 @@
+﻿module Task6
+
+let main (inArray:array<int>) = 
+    inArray.[0] <- inArray.[0] + inArray.[1] 
+    inArray.[1] <- inArray.[0] - inArray.[1]
+    inArray.[0] <- inArray.[0] - inArray.[1]
+   
+    printfn "%A" inArray
+    inArray

07f10768a041f00f4b12e7bffbbd8241deb2d22d
KochetovKirill <kirya.god@yandex.ru>
2015-09-21 19:42:24 +0300

    task 10 deleted

--- 2015/143/KochetovKirill/src/Task10/Program.fs
+++ 2015/143/KochetovKirill/src/Task10/Program.fs
@@ -1,17 +1,15 @@
-﻿module Task10
-
-  let main n = 
-    let next c d =
-      for a in 3..n do
-        c = c + d
-        d = c - d
-    next 1 1
-
-  let n = System.Console.ReadLine() |> int
-
-  [<EntryPoint>]
-  let inter argv = 
-    printfn "%A" <| main n
-    let rk = System.Console.ReadKey(true)
-    0
+﻿//module Task10
+//
+//  let main n = 
+//      for a in 3..n do
+//        next c d
+//    next 1 1
+//  
+//  let n = System.Console.ReadLine() |> int
+//
+//  [<EntryPoint>]
+//  let inter argv = 
+//    printfn "%A" <| main n
+//    let rk = System.Console.ReadKey(true)
+//    0
     

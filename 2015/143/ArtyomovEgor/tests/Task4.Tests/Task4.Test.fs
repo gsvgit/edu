f774bfad72b48d5ea93ce6676178f258f4e4661f
ArtyomovEgor <art-y@inbox.ru>
2015-09-24 22:16:14 +0300

    Revert "Formatted 4, 5, 6"

--- 2015/143/ArtyomovEgor/tests/Task4.Tests/Task4.Test.fs
+++ 2015/143/ArtyomovEgor/tests/Task4.Tests/Task4.Test.fs
@@ -11,14 +11,15 @@ let ``main [||] 10 return [||]`` () =
 [<Test>]
 let ``main [|0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10|] 5 return [|0; 1; 2; 3; 4; 5|]`` () =
   let res = main [|0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10|] 5
+  printfn "%A" res
   Assert.AreEqual([|0; 1; 2; 3; 4; 5|], res)
 
 [<Test>]
 let ``main [|10; 9; 8; 7; 6; 5; 4; 3; 2; 1; 0|] 7 return [|3; 4; 5; 6; 7; 8; 9; 10|]`` () =
-  let res = main [|10; 9; 8; 7; 6; 5; 4; 3; 2; 1; 0|] 7
+  let res = main  [|10; 9; 8; 7; 6; 5; 4; 3; 2; 1; 0|] 7
   Assert.AreEqual([|3; 4; 5; 6; 7; 8; 9; 10|], res)
 
 [<Test>]
 let ``main [|100; 900; 8750; 7; 61; 587|] 0 return [||]`` () =
-  let res = main [|100; 900; 8750; 7; 61; 587|] 0
+  let res = main  [|100; 900; 8750; 7; 61; 587|] 0
   Assert.AreEqual([||], res)
--- 2015/143/ArtyomovEgor/tests/Task5.Tests/Task5.Tests.fs
+++ 2015/143/ArtyomovEgor/tests/Task5.Tests/Task5.Tests.fs
@@ -11,14 +11,15 @@ let ``main [||] 1 10 return [||]`` () =
 [<Test>]
 let ``main [|0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10|] 3 8 return [|0; 1; 2; 9; 10|]`` () =
   let res = main [|0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10|] 3 8
+  printfn "%A" res
   Assert.AreEqual([|0; 1; 2; 9; 10|], res)
 
 [<Test>]
 let ``main [|10; 9; 8; 7; 6; 5; 4; 3; 2; 1; 0|] 3 8 return [|0; 1; 2; 8; 9; 10|]`` () =
-  let res = main [|10; 9; 8; 7; 6; 5; 4; 3; 2; 1; 0|] 3 8
+  let res = main  [|10; 9; 8; 7; 6; 5; 4; 3; 2; 1; 0|] 3 8
   Assert.AreEqual([|0; 1; 8; 9; 10|], res)
 
 [<Test>]
 let ``main [|100; 900; 8750; 7; 61; 587|] 0 10000 return [||]`` () =
-  let res = main [|100; 900; 8750; 7; 61; 587|] 0 10000
+  let res = main  [|100; 900; 8750; 7; 61; 587|] 0 10000
   Assert.AreEqual([||], res)

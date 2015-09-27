bfd5262f7015db3997bb68280974e95af2f27659
SusaninaJulia <u.a.susxv@mail.ru>
2015-09-27 21:59:26 +0300

    Revert "2-14"
    This reverts commit 126f48c5a10ebacd0b2ec6240c5ef72fb30e5642.

---
+++ 2015/143/SusaninaJulia/src/Task4/Task4/Task4.fs
@@ -0,0 +1,26 @@
+﻿module Task4
+
+let main (inArray:array<int>) (num:int) = 
+  
+
+(* 
+let genRandomNumbers count =
+    let rnd = System.Random()
+    Array.init count (fun _ -> rnd.Next (1, 15)) // Lambda-expression
+
+let inArray = genRandomNumbers 10
+printfn "%A" inArray
+
+let x = System.Console.ReadLine()
+let num = int x 
+*)
+
+
+    let mutable L = []
+
+    for i in 0..inArray.Length - 1 do
+        if num >= inArray.[i] then 
+            L <- i :: L
+    let indL = L
+    let outArray = [|for i in List.rev(indL) -> i|] 
+    outArray
--- 2015/143/SusaninaJulia/tests/Task8.Tests/Script.fsx
+++
@@ -1,8 +0,0 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
-open Task8.Tests
-
-// Define your library scripting code here
-

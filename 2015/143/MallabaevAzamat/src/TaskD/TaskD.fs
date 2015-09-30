6018335e5d16523a7b33329ce49df1795363e079
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-09-30 14:06:29 +0300

    Updated 12, 14 solutions

--- 2015/143/MallabaevAzamat/src/TaskD/TaskD.fs
+++ 2015/143/MallabaevAzamat/src/TaskD/TaskD.fs
@@ -1,28 +1,19 @@
 ﻿module TaskD
 
-let (>*>) (a:int array array) (b:int array array) =
-  [|[|a.[0].[0] * b.[0].[0] + a.[1].[0] * b.[0].[1];
-      a.[0].[0] * b.[1].[0] + a.[1].[0] * b.[1].[1]|];
-    [|a.[0].[1] * b.[0].[0] + a.[1].[1] * b.[0].[1];
-      a.[0].[1] * b.[1].[0] + a.[1].[1] * b.[1].[1]|]|]
-
-let rec sqrt (a:int array array) = a >*> a
-
-let rec (>^>) (a:int array array) n =
-  if n = 1 then a
-  else if n % 2 = 0 then (sqrt a) >^> n / 2
-  else a >*> (a >^> (n - 1))
-
-let fib n =
-  if n = 0 then 0
-  else if n > 0 then ([|[|1; 1|];
-                        [|1; 0|]|] >^> n).[0].[1]
-  else if (-n) % 2 = 1 then ([|[|1; 1|];
-                               [|1; 0|]|] >^> -n).[0].[1]
-  else -([|[|1; 1|];
-           [|1; 0|]|] >^> -n).[0].[1]
-
 let main n =
   if n = 0 then [|0|]
-  else if n > 0 then [|for i in 0..n -> fib i|]
-  else [|for i in 0..-n -> fib -i|]
+  else if n = 1 || n = -1 then [|0; 1|]
+  else if n > 0 then
+    let fib = [|for _ in 0..n -> 0|]
+    fib.[0] <- 0
+    fib.[1] <- 1
+    for i in 2..n do
+      fib.[i] <- fib.[i - 1] + fib.[i - 2]   
+    fib
+  else
+    let fib = [|for _ in 0..-n -> 0|]
+    fib.[0] <- 0
+    fib.[1] <- 1
+    for i in 2..-n do
+      fib.[i] <- fib.[i - 2] - fib.[i - 1]   
+    fib

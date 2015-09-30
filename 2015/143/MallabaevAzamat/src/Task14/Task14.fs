edb20c468658054aaa2f9f4f586b22643a38a3ef
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-09-30 16:56:54 +0300

    Update 14

--- 2015/143/MallabaevAzamat/src/Task14/Task14.fs
+++ 2015/143/MallabaevAzamat/src/Task14/Task14.fs
@@ -1,27 +1,35 @@
 ﻿module Task14
 
+open System.Drawing
+open System.Windows.Forms
 open FSharp.Charting
 
 let once_time i f =
   let time_start = System.DateTime.UtcNow
   ignore <| f i
-  (System.DateTime.UtcNow - time_start).TotalMilliseconds
+  (System.DateTime.UtcNow - time_start).TotalMilliseconds |> int
 
-let many_time count i f =
-  sqrt ((List.sum [for _ in 0..count -> (once_time i f) ** 2.0]) / (float count))
+let many_time i f =
+  let marr = (Array.sort [|for _ in 0..8 -> (once_time i f)|])
+  (marr.[0] + marr.[1] * 2 + marr.[2] * 3 + marr.[3] * 5 + marr.[4] * 8 + marr.[5] * 5 + marr.[6] * 3 + marr.[7] * 2 + marr.[8]) / 30
 
 [<EntryPoint>]
 let entry args =
-  let redo_count = 10
-  let fib_graph = Chart.Line([for i in -39..39 -> (i, TaskC.main i)], Title="Fibonacci function").WithXAxis(Title="N").WithYAxis(Title="fib N")
-  let ``Task8.main N`` = Chart.Line [for i in -39..39 -> (i, many_time redo_count i Task8.main)]
-  let ``Task9.main N`` = Chart.Line [for i in -39..39 -> (i, many_time redo_count i Task9.main)]
-  let ``TaskA.main N`` = Chart.Line [for i in -39..39 -> (i, many_time redo_count i TaskA.main)]
-  let ``TaskB.main N`` = Chart.Line [for i in -39..39 -> (i, many_time redo_count i TaskB.main)]
-  let ``TaskC.main N`` = Chart.Line [for i in -39..39 -> (i, many_time redo_count i TaskC.main)]
-  let tasks_stat = (Chart.Combine([``Task8.main N``; ``Task9.main N``; ``TaskA.main N``; ``TaskB.main N``; ``TaskC.main N``]).WithTitle "Tasks time").WithXAxis(Title="N").WithYAxis(Title="milliseconds")
-  let Table = Chart.Rows [fib_graph; tasks_stat]
-  
-  System.Windows.Forms.Application.Run(Table.ShowChart())
+  let fib_graph = Chart.Line([for i in -50..50 -> (i, TaskC.main i)], Title="Fibonacci function").WithXAxis(Title="N").WithYAxis(Title="fib N")
+  let ``Task8.main N graph`` = Chart.Line ([for i in -30..30 -> (i, many_time i Task8.main)], Color = Color.Black)
+  let ``Task9.main N graph`` = Chart.Line ([for i in 0..100000..10000000 -> (i, many_time i Task9.main)], Color = Color.Gold)
+  let ``TaskA.main N graph`` = Chart.Line ([for i in 0..100000..10000000 -> (i, many_time i TaskA.main)], Color = Color.Green)
+  let ``TaskB.main N graph`` = Chart.Line ([for i in 0..100000..10000000 -> (i, many_time i TaskB.main)], Color = Color.Blue)
+  let ``TaskC.main N graph`` = Chart.Line ([for i in 0..100000..10000000 -> (i, many_time i TaskC.main)], Color = Color.Red)
+  let tasks_stat = (Chart.Combine([
+     ``Task8.main N graph``;
+     ``Task9.main N graph``;
+     ``TaskA.main N graph``; 
+     ``TaskB.main N graph``; 
+     ``TaskC.main N graph``]).WithTitle "Tasks time\n8 - Black, 9 - Gold, 10 - Green, 11 - Blue, 12 - Red").WithXAxis(Title="N").WithYAxis(Title="milliseconds")
+  let table = Chart.Rows [fib_graph; tasks_stat]
+  let window = table.ShowChart()
+
+  Application.Run(window)
   
   0

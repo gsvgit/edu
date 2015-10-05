6dabfc58a1a33d00dddaf80695f63843c3d997d6
ArtyomovEgor <art-y@inbox.ru>
2015-10-06 00:29:44 +0300

    again(

---
+++ 2015/143/ArtyomovEgor/src/Task9/Task9.fs
@@ -0,0 +1,13 @@
+﻿module Task9
+
+let fibN n =
+    let mutable a = 1
+    let mutable b = 1
+    // if n < 1 then 0
+    //elif n <= 2 then 1
+    //else 
+    for i in 1..n-1 do
+        a <- a + b          //a1=2 b1=1  
+        b <- a - b          //a2=2 b2=1
+    b                       //a3=3 b3=2
+                            //a4=5 b4=3

80e0c80e25849be3554a6d0400bf588ff0233e9b
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-10-09 17:37:24 +0300

    task19 solution updated

--- 2015/143/MallabaevAzamat/src/Task19/Task19.fs
+++ 2015/143/MallabaevAzamat/src/Task19/Task19.fs
@@ -3,17 +3,43 @@
 open Task15
 
 type 'a Tree =
+| Node of 'a * 'a TreeList
 | Leaf of 'a
-| Node of 'a * 'a Tree List
-             
-let rec max_min tree =
-  let rec _max_min tree_list (max, min) =
-    match tree_list with
-    | Nil -> (max, min)
-    | Cons(x, s) ->
-        let (m1, m2) = max_min x
-        _max_min s ((if m1 > max then m1 else max), (if m2 < min then m2 else min))
 
-  match tree with
-  | Leaf(x) -> (x, x)
-  | Node(x, s) -> _max_min s (x, x)
+and 'a TreeList = 'a Tree List            
+
+let rec fold start f = function
+  | Nil -> start
+  | Cons(hd, tl) -> fold (f start hd) f tl
+
+let map f = fold Nil <| fun st li -> Cons(f li, st)
+let forAll f = fold () <| fun _ li -> f li
+
+let max a b = match a > b with | true -> a | _ -> b
+let min a b = match a < b with | true -> a | _ -> b
+
+type Result<'okType> =
+| Ok of 'okType
+| Fail
+
+let rec max_min = function
+  | Node(_, lst) ->
+      let rec lstmm st l = l |> fold st (function
+        | Fail -> function
+          | Node(_, lst) -> lstmm Fail lst
+          | Leaf a -> Ok (a, a)
+        | Ok((mx, mn)) -> function
+          | Node(_, lst) -> lstmm st lst
+          | Leaf a -> Ok ((max a mx), min a mn))
+      lstmm Fail lst
+  | Leaf a -> Ok (a, a)
+
+let rT2Str = function
+| Ok (a, b) -> sprintf "Ok : (%s, %s)" (a.ToString()) (b.ToString())
+| Fail -> "Fail"
+
+[<EntryPoint>]
+let entry args =
+  printfn "%s" <| rT2Str (max_min <| Node (1, Cons(Leaf 10, Cons(Leaf 1, Nil))))
+  ignore <| System.Console.ReadLine()
+  0
--- 2015/143/MallabaevAzamat/tests/Task19.Tests/Task19.Tests.fs
+++ 2015/143/MallabaevAzamat/tests/Task19.Tests/Task19.Tests.fs
@@ -4,19 +4,19 @@ open Task15
 open Task19
       
 [<Test>]                       
-let ``max_min Leaf 0 returned (0, 0)`` () =
+let ``max_min Leaf 0 returned Ok(0, 0)`` () =
   let result = max_min <| Leaf 0
   printfn "%A" result
-  Assert.AreEqual((0, 0), result)
+  Assert.AreEqual(Ok(0, 0), result)
 
 [<Test>]
-let ``max_min(Node(0, (Cons(Leaf 1, Nil)))) returned (1, 0)`` () =
-  let result = max_min <| Node(0, Cons(Leaf 1, Nil))
+let ``max_min (Node(0, (Cons(Leaf 1, Cons(Leaf 10, Nil)))))) returned Ok(1, 1)`` () =
+  let result = max_min <| (Node(0, (Cons(Leaf 1, Cons(Leaf 10, Nil)))))
   printfn "%A" result
-  Assert.AreEqual((1, 0), result)
+  Assert.AreEqual(Ok(10, 1), result)
 
 [<Test>]
-let ``max_min Node(Leaf ...) returned (42, -10)`` () =
-  let result = max_min <| Node (-10, Cons((Leaf 1), Cons(Node(42, Nil), Nil)))
+let ``max_min (Node (-10, Cons (Node (42, Nil), Nil))) returned Fail`` () =
+  let result: Result<int * int> = max_min <| Node (-10, Cons (Node (42, Nil), Nil))
   printfn "%A" result
-  Assert.AreEqual((42, -10), result)
+  Assert.AreEqual(Result<int * int>.Fail, result)

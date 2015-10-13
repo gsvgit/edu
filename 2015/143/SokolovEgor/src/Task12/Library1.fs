aa76a3e035e2bb9fa9d0407550c10e1e4e386672
SokolovEgor <egos1700@gmail.com>
2015-10-13 18:48:42 +0300

    Add 12

---
+++ 2015/143/SokolovEgor/src/Task12/Library1.fs
@@ -0,0 +1,22 @@
+﻿module Task12
+
+open Task11
+
+let rec Result (Array:int[,]) n =
+    if n = 1 || n = 0 
+    then
+        Array
+    else
+        if n % 2 = 0 
+        then
+            let res = func (Result Array (n / 2)) (Result Array (n / 2))
+            res
+        else
+            let rc = func (Result Array ((n - 1) / 2)) (Result Array ((n - 1) / 2))
+            let res = func Array rc
+            res
+ 
+let main n = 
+    let mt = array2D [ [1; 1]; [1; 0] ]
+    let res = Result mt (n - 1)
+    res.[0,0]

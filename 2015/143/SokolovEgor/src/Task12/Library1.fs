f6efa97ea9f9cae942b7c773dc6ccebf82a78901
SokolovEgor <egos1700@gmail.com>
2015-10-15 00:25:49 +0300

    Add 19; Edit 1: del "num" in task and tests for fix warnings

--- 2015/143/SokolovEgor/src/Task12/Library1.fs
+++ 2015/143/SokolovEgor/src/Task12/Library1.fs
@@ -2,21 +2,21 @@
 
 open Task11
 
-let rec Result (Array:int[,]) n =
+let rec result (array:int[,]) n =
     if n = 1 || n = 0 
     then
-        Array
+        array
     else
         if n % 2 = 0 
         then
-            let res = func (Result Array (n / 2)) (Result Array (n / 2))
+            let res = func (result array (n / 2)) (result array (n / 2))
             res
         else
-            let rc = func (Result Array ((n - 1) / 2)) (Result Array ((n - 1) / 2))
-            let res = func Array rc
+            let rc = func (result array ((n - 1) / 2)) (result array ((n - 1) / 2))
+            let res = func array rc
             res
  
 let main n = 
     let mt = array2D [ [1; 1]; [1; 0] ]
-    let res = Result mt (n - 1)
+    let res = result mt (n - 1)
     res.[0,0]
---
+++ 2015/143/SokolovEgor/src/Task19/Library1.fs
@@ -0,0 +1,44 @@
+﻿module Task19
+
+open Task15
+
+type Tree = 
+    | Node of int * List<Tree>
+    | Leaf of int
+
+let main treeB =
+    let init tree =
+        match tree with
+        | Leaf (sign) -> sign
+        | Node (sign, tree) -> sign
+
+    let rec min treeInput minimum =
+        match treeInput with
+        | Leaf (sign) -> 
+            if sign > minimum 
+            then minimum
+            else sign
+        | Node (sign, tree) ->
+            let rec minInLst tree minimumInLst =
+                match tree with
+                | Empty -> minimumInLst
+                | Cons (head, tale) ->
+                    min head (minInLst tale minimumInLst)
+            if sign < minInLst tree minimum 
+            then sign
+            else minInLst tree minimum
+    let rec max treeInput maximum =
+        match treeInput with
+        | Leaf (sign) -> 
+            if sign < maximum 
+            then maximum
+            else sign
+        | Node (sign, tree) ->
+            let rec maxInLst tree maximumInLst =
+                match tree with
+                | Empty -> maximumInLst
+                | Cons (head, tale) -> max head (maxInLst tale maximumInLst)
+            if sign > maxInLst tree maximum 
+            then sign
+            else maxInLst tree maximum
+    max treeB (init treeB), min treeB (init treeB)

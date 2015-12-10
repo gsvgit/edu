8d2387e2fbfb73c56be901cf6431ab7b418938b5
SokolovEgor <egos1700@gmail.com>
2015-12-10 23:43:07 +0300

    Rewrite 10, 12, 13, 14

--- 2015/143/SokolovEgor/src/Task14/Program.fs
+++ 2015/143/SokolovEgor/src/Task14/Program.fs
@@ -14,9 +14,9 @@ let main =
         [
              Chart.Line( [ for i in 10..2..30 -> (i, time(fun () -> Task8.main i |> ignore) ) ], "Rec. method", Color = System.Drawing.Color.Black)
              Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task9.main i |> ignore) ) ], "Iter. method", Color = System.Drawing.Color.Gold)
-             Chart.Line( [ for i in 1..1000..70000 -> (i, time(fun () -> Task10.main i |> ignore) ) ], "Iter. method without mutable", Color = System.Drawing.Color.Gray)
-             Chart.Line( [ for i in 1..1000..20000 -> (i, time(fun () -> Task11.main i |> ignore) ) ], "Matrix, easy method", Color = System.Drawing.Color.Blue)
-             Chart.Line( [ for i in 1..1000..30000 -> (i, time(fun () -> Task12.main i |> ignore) ) ], "Matrix, log", Color = System.Drawing.Color.Green)
+             Chart.Line( [ for i in 1..1000..90000 -> (i, time(fun () -> Task10.main i |> ignore) ) ], "Iter. method without mutable", Color = System.Drawing.Color.Gray)
+             Chart.Line( [ for i in 1..1000..10000 -> (i, time(fun () -> Task11.main i |> ignore) ) ], "Matrix, easy method", Color = System.Drawing.Color.Blue)
+             Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task12.main i |> ignore) ) ], "Matrix, log", Color = System.Drawing.Color.Green)
              Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task13.main i |> ignore) ) ], "Array", Color = System.Drawing.Color.Red)
         ]
 do System.Windows.Forms.Application.Run(main.ShowChart())

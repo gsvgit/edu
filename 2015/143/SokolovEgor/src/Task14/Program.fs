0a804c66471e97f517251e2d55aab1547f06da3d
SokolovEgor <egos1700@gmail.com>
2015-12-12 01:11:07 +0300

    14 edit

--- 2015/143/SokolovEgor/src/Task14/Program.fs
+++ 2015/143/SokolovEgor/src/Task14/Program.fs
@@ -9,7 +9,7 @@ let time f =
     for i = 1 to 20 do f()
     (System.DateTime.Now - start).TotalMilliseconds / 20.00
 
-let main =
+let charting =
     Chart.Combine
         [
              Chart.Line( [ for i in 10..2..30 -> (i, time(fun () -> Task8.main i |> ignore) ) ], "Rec. method", Color = System.Drawing.Color.Black)
@@ -19,4 +19,4 @@ let main =
              Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task12.main i |> ignore) ) ], "Matrix, log", Color = System.Drawing.Color.Green)
              Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task13.main i |> ignore) ) ], "Array", Color = System.Drawing.Color.Red)
         ]
-do System.Windows.Forms.Application.Run(main.ShowChart())
+do System.Windows.Forms.Application.Run(charting.ShowChart())

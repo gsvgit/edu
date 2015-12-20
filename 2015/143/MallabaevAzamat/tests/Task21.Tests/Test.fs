45c755bcbe30d06650636346ae0dc6f6c3a4e706
Fenex9428 <mallabaev.an@yandex.ru>
2015-12-20 21:59:26 +0300

    Recovery fix

--- 2015/143/MallabaevAzamat/tests/Task21.Tests/Test.fs
+++ 2015/143/MallabaevAzamat/tests/Task21.Tests/Test.fs
@@ -6,23 +6,23 @@ open System
 open NUnit.Framework
 
 [<Test>]
-let ``Task21:main Empty = Smth Empty)``() =
+let ``Task21:main Empty = Empty)``() =
     let arg = Empty
     let result = main arg
     printf "main %A = %A" arg result
-    Assert.AreEqual(result, Empty)
+    Assert.IsTrue(match result with | Empty -> true | _ -> false)
 
 [<Test>]
 let ``Task21:main (Smth (12, Smth (15, Smth (31, Smth (42, Empty))))) = Smth (12, Smth (15, Smth (31, Smth (42, Empty))))``() =
     let arg = Smth (12, Smth (15, Smth (31, Smth (42, Empty))))
     let result = main arg
     printf "main %A = %A" arg result
-    Assert.AreEqual(result, Smth (12, Smth (15, Smth (31, Smth (42, Empty)))))
+    Assert.AreEqual(Smth (12, Smth (15, Smth (31, Smth (42, Empty)))), result)
 
 [<Test>]
 let ``Task21:main (Smth (15, Smth (12, Smth (42, Smth (31, Empty))))) = Smth (12, Smth (15, Smth (31, Smth (42, Empty))))``() =
     let arg = (Smth (15, Smth (12, Smth (42, Smth (31, Empty)))))
     let result = main arg
     printf "main %A = %A" arg result
-    Assert.AreEqual(result, Smth (12, Smth (15, Smth (31, Smth (42, Empty)))))
+    Assert.AreEqual(Smth (12, Smth (15, Smth (31, Smth (42, Empty)))), result)
 

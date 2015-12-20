c3a47d78f7e673ec67a8a9814a7c4e5346bade5b
Fenex9428 <mallabaev.an@yandex.ru>
2015-12-21 02:45:21 +0300

    Added 18

--- 2015/143/MallabaevAzamat/src/Task15/AssemblyInfo.fs
+++ 2015/143/MallabaevAzamat/src/Task15/AssemblyInfo.fs
@@ -1,20 +1,12 @@
-﻿namespace Task15
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
 
-[<assembly: AssemblyTitle("Task15")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("")>]
-[<assembly: AssemblyCopyright("konstantin")>]
-[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyTitleAttribute("Task15")>]
+[<assembly: AssemblyProductAttribute("MallabaevAzamatHW")>]
+[<assembly: AssemblyDescriptionAttribute("Home work")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
 
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
-
-[<assembly: AssemblyVersion("1.0.0.0")>]
-
-//[<assembly: AssemblyDelaySign(false)>]
-//[<assembly: AssemblyKeyFile("")>]
-
-()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ 2015/143/MallabaevAzamat/src/Task18/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Task18")>]
+[<assembly: AssemblyProductAttribute("MallabaevAzamatHW")>]
+[<assembly: AssemblyDescriptionAttribute("Home work")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"

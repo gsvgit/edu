f3dcd954f88638a36feed8957a875f5e8ca11a09
KochetovKirill <kirya.god@yandex.ru>
2015-12-22 18:47:12 +0300

    Tasks 27, 28, 29 added

--- 2015/143/KochetovKirill/src/Task29/Program.fs
+++ 2015/143/KochetovKirill/src/Task29/Program.fs
@@ -1,60 +1,25 @@
 ﻿module Task29
 
-let trans (x : int64) = 
-    let mutable n = x
-    let a = Array.zeroCreate 64
-    let mutable i = 63
-    while n > 0L do
-        a.[i] <- n % 2L
-        i <- i - 1
-        n <- n / 2L
-    a
+let main number=
+    let mutable nm = number
+    let mutable bit = ""
 
-let pres (x : int64) = 
-    let mutable n = x
-    if n >= 0L 
-    then trans x
-    else 
-        n <- -1L * n
-        let mutable temp = trans n
-        for i in 0..63 do
-            if temp.[i] = 1L
-            then 
-                temp.[i] <- 0L
-            else 
-                temp.[i] <- 1L
-        let mutable a = 1
-        temp <- Array.rev temp
-        for i in 0..63 do
-            if a = 1 
-            then 
-                if temp.[i] = 0L
-                then 
-                    temp.[i] <- 1L
-                    a <- 0
-                else temp.[i] <- 0L
-        temp <- Array.rev temp
-        temp
-
-let result n =
-    if n = -9223372036854775808L
+    if nm < 0L
     then
-        let g = [|for i in 0..62 -> 0L|]
-        Array.append [|1L|] g
-    else
-        pres n
+        nm <- nm &&&  System.Int64.MaxValue
 
-let main n = 
-    if n = -9223372036854775808L 
-    then 
-        let g = [|for i in 0..62 -> 0L|]
-        let f = Array.append [|1L|] g
-        for i in 0..63 do 
-            printfn "%d" f.[i]
-    else
-        let g = pres n
-        for i in 0..63 do
-            printf "%d" g.[i]
-        printf "\n"
-let reading = System.Console.ReadLine() |> int64
-System.Console.WriteLine(main reading)
+    for i in 0..62 do
+        bit <- ((nm % 2L) |> string) + bit
+        nm <- (nm - nm % 2L)/2L
+
+    if number < 0L
+    then bit <- "1" + bit
+    else bit <- "0" + bit
+
+    bit
+
+[<EntryPoint>]
+let inter argv =
+    printf "%s" (main (System.Console.ReadLine() |> int64))
+    let rk = System.Console.ReadKey(true)
+    0

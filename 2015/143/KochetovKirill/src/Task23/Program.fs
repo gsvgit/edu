fdb09b1a334af08d009e106b080551bf4c8f6605
KochetovKirill <kirya.god@yandex.ru>
2015-12-22 15:47:49 +0300

    Task 19 edited, task 23 edited

--- 2015/143/KochetovKirill/src/Task23/Program.fs
+++ 2015/143/KochetovKirill/src/Task23/Program.fs
@@ -6,12 +6,14 @@ let main (inArr : array<int>) =
         [||]
     else
         let rec quickSort (Arr : array<int>) (l : int) (h : int) =
-            let x = Arr.[(l + h)/2]
+            let x = Arr.[(l + h) / 2]
             let mutable i = l
             let mutable j = h
             while i < j do
-                while Arr.[i] < x do i <- i + 1
-                while Arr.[j] > x do j <- j - 1
+                while Arr.[i] < x do 
+                    i <- i + 1
+                while Arr.[j] > x do 
+                    j <- j - 1
                 if i <= j 
                 then
                     let t = Arr.[j]
@@ -19,9 +21,10 @@ let main (inArr : array<int>) =
                     Arr.[i] <- t
                     i <- i + 1
                     j <- j - 1
-                    printfn "%A" <| Arr
-            if l < j then quickSort Arr l j
-            if i < h then quickSort Arr i h
+            if l < j 
+            then quickSort Arr l j
+            if i < h 
+            then quickSort Arr i h
         quickSort inArr 0 (inArr.Length - 1)
         inArr
 

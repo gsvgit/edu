26185e6f49a3c3ee15379a3f91e8e71129694fa0
ArtyomovEgor <art-y@inbox.ru>
2015-12-22 15:23:47 +0300

    14,24

--- 2015/143/ArtyomovEgor/src/Task14/Program.fs
+++ 2015/143/ArtyomovEgor/src/Task14/Program.fs
@@ -14,14 +14,14 @@ let time f =
     (System.DateTime.Now - start).TotalMilliseconds / 10.0
 
 let gch = 
-    Chart.Combine
+    (Chart.Combine
         [
-             Chart.Line( [ for i in 10..2..30 -> (i, time(fun () -> Task8.main i |> ignore) ) ], "рекурсия", Color = System.Drawing.Color.Yellow)
+             Chart.Line( [ for i in 1..2..30 -> (i, time(fun () -> Task8.main i |> ignore) ) ], "рекурсия", Color = System.Drawing.Color.Yellow)
              Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task9.main i |> ignore) ) ], "итеративно", Color = System.Drawing.Color.Green)
-             Chart.Line( [ for i in 1..1000..90000 -> (i, time(fun () -> Task10.main i |> ignore) ) ], "итеративно, функционально, волшебно", Color = System.Drawing.Color.Gray)
-             Chart.Line( [ for i in 1..1000..10000 -> (i, time(fun () -> Task11.main i |> ignore) ) ], "матрицы", Color = System.Drawing.Color.Blue)
-             Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task12.main i |> ignore) ) ], "матрицы за логарифм", Color = System.Drawing.Color.Black)
-             Chart.Line( [ for i in 1..1000..100000 -> (i, time(fun () -> Task13.main i |> ignore) ) ], "через массив", Color = System.Drawing.Color.Red)
-        ]
+             Chart.Line( [ for i in 1..100..300 -> (i, time(fun () -> Task10.main i |> ignore) ) ], "итеративно, функционально, волшебно", Color = System.Drawing.Color.Gray)
+             Chart.Line( [ for i in 1..1000..5000 -> (i, time(fun () -> Task11.main i |> ignore) ) ], "матрицы", Color = System.Drawing.Color.Blue)
+             Chart.Line( [ for i in 1..1000..50000 -> (i, time(fun () -> Task12.main i |> ignore) ) ], "матрицы за логарифм", Color = System.Drawing.Color.Black)
+             Chart.Line( [ for i in 1..1000..50000 -> (i, time(fun () -> Task13.main i |> ignore) ) ], "через массив", Color = System.Drawing.Color.Red)
+        ])
 
 do System.Windows.Forms.Application.Run(gch.ShowChart())
--- 2015/143/ArtyomovEgor/src/Task24/Program.fs
+++ 2015/143/ArtyomovEgor/src/Task24/Program.fs
@@ -23,11 +23,11 @@ let time f =
 let gch =
     Chart.Combine
         [
-            Chart.Line( [ for i in 0..10..600 -> (i, time(fun () -> Task21.main (myListRandom i) |> ignore) ) ], "bubblelsort", Color = System.Drawing.Color.Black) 
-            Chart.Line( [ for i in 0..10..350 -> (i, time(fun () -> Task22.main (myListRandom i) |> ignore) ) ], "qsort MyList", Color = System.Drawing.Color.Lime) 
-            Chart.Line( [ for i in 0..10..1300 -> (i, time(fun () -> Task23.main [|for k in 1..i -> System.Random().Next()|] |> ignore) ) ], "qsort ", Color = System.Drawing.Color.Red) 
-            Chart.Line( [ for i in 0..10..1300 -> (i, time(fun () -> Array.sort([|for k in 1..i -> System.Random().Next()|]) |> ignore) ) ], "array sort", Color = System.Drawing.Color.Gold) 
-            Chart.Line( [ for i in 0..10..1300 -> (i, time(fun () -> List.sort([for k in 1..i ->System.Random().Next()]) |> ignore) ) ], "list sort", Color = System.Drawing.Color.Magenta) 
+            Chart.Line( [ for i in 0..100..600 -> (i, time(fun () -> Task21.main (myListRandom i) |> ignore) ) ], "bubblelsort", Color = System.Drawing.Color.Black) 
+            Chart.Line( [ for i in 0..100..350 -> (i, time(fun () -> Task22.main (myListRandom i) |> ignore) ) ], "qsort MyList", Color = System.Drawing.Color.Lime) 
+            Chart.Line( [ for i in 0..100..1300 -> (i, time(fun () -> Task23.main [|for k in 1..i -> System.Random().Next()|] |> ignore) ) ], "qsort ", Color = System.Drawing.Color.Red) 
+            Chart.Line( [ for i in 0..100..1300 -> (i, time(fun () -> Array.sort([|for k in 1..i -> System.Random().Next()|]) |> ignore) ) ], "array sort", Color = System.Drawing.Color.Gold) 
+            Chart.Line( [ for i in 0..100..1300 -> (i, time(fun () -> List.sort([for k in 1..i ->System.Random().Next()]) |> ignore) ) ], "list sort", Color = System.Drawing.Color.Magenta) 
         ]
 
 do System.Windows.Forms.Application.Run(gch.ShowChart())

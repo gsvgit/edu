b779ab71f79d92a78a8d5a95b19519afb8e83e3c
SusaninaJulia <u.a.susxv@mail.ru>
2015-12-27 20:09:37 +0300

    32

--- 2015/143/SusaninaJulia/src/Task32/Task32.fs
+++ 2015/143/SusaninaJulia/src/Task32/Task32.fs
@@ -8,6 +8,9 @@ open FSharp.Charting
 open System.Drawing
 open System.Windows.Forms
 
+let toNum1 () = (1, Cons(1, Empty))
+let toNum0 () = (1, Cons(0, Empty))
+
 let ptn (x : Num) = 
     match x with 
     | (s, n) ->
@@ -16,49 +19,45 @@ let ptn (x : Num) =
         else (1, n)
 
 let rec task8FibN (n : Num) =
-    if n = (1, Cons(1, Empty)) || n = (1, Cons(2, Empty))
-    then (1, Cons(1, Empty))
+    if n = toNum1 () || n = (1, Cons(2, Empty))
+    then toNum1 ()
     else sum (task8FibN (sum n (-1, Cons(1, Empty))))  (task8FibN (sum n (-1, Cons(2, Empty))))
 
 
 let task9FibN (n : Num) = 
-    let toNum1 = (1, Cons(1, Empty))
-    let mutable a = toNum1
-    let mutable b = toNum1
+    let mutable a = toNum1 ()
+    let mutable b = toNum1 ()
     let mutable i = (1, Cons(2, Empty))
     while i.Equals(n) = false do
         a <- sum a b 
         b <- sum a (ptn b)
-        i <- sum i toNum1
+        i <- sum i (toNum1 ())
     a
 
 let task10FibN (n : Num) = 
-    let toNum1 = (1, Cons(1, Empty))
     let rec fibN n fib1 fib2 i = 
-        if n = toNum1 || n = (1, Cons(2, Empty)) 
-        then toNum1
+        if n = toNum1 () || n = (1, Cons(2, Empty)) 
+        then toNum1 ()
         elif n = i
         then fib1
-        else fibN n (sum fib1 fib2) fib1 (sum i toNum1)
-    fibN n toNum1 toNum1 (1, Cons(2, Empty))
+        else fibN n (sum fib1 fib2) fib1 (sum i (toNum1 ()))
+    fibN n (toNum1 ()) (toNum1 ()) (1, Cons(2, Empty))
 
 let task11FibN (n : Num) = 
-    let toNum1 = (1, Cons(1, Empty))
-    let toNum0 = (1, Cons(0, Empty))
-    let mutable arr1 = [|[|toNum1; toNum1|]; [|toNum1; toNum0|]|]
-    let arr2 = [|[|toNum1; toNum1|]; [|toNum1; toNum0|]|]
-    let mutable e00 = toNum0
-    let mutable e01 = toNum0
-    let mutable e10 = toNum0
-    let mutable e11 = toNum0
-    let mutable i = toNum1
+    let mutable arr1 = [|[|toNum1 (); toNum1 ()|]; [|toNum1 (); toNum0 ()|]|]
+    let arr2 = [|[|toNum1 (); toNum1 ()|]; [|toNum1 (); toNum0 ()|]|]
+    let mutable e00 = toNum0 ()
+    let mutable e01 = toNum0 ()
+    let mutable e10 = toNum0 ()
+    let mutable e11 = toNum0 ()
+    let mutable i = toNum1 ()
     while i.Equals(n) = false do 
         e00 <- sum (multiplication arr1.[0].[0] arr2.[0].[0]) (multiplication arr1.[0].[1] arr2.[1].[0])
         e01 <- sum (multiplication arr1.[0].[0] arr2.[0].[1]) (multiplication arr1.[0].[1] arr2.[1].[1])
         e10 <- sum (multiplication arr1.[1].[0] arr2.[0].[0]) (multiplication arr1.[1].[1] arr2.[1].[0])
         e11 <- sum (multiplication arr1.[1].[0] arr2.[0].[1]) (multiplication arr1.[1].[1] arr2.[1].[1])
         arr1 <- [|[|e00; e01|];[|e10; e11|]|]
-        i <- sum i toNum1
+        i <- sum i (toNum1 ())
     arr1.[0].[1]
 
 let mod2 (n : Num) = 
@@ -104,41 +103,38 @@ let inInt n =
         s * (inInt1 lst 0)
 
 
+let matrixMultiply (arr1 : Num[][]) (arr2 : Num[][]) = 
+    let e00 = sum (multiplication arr1.[0].[0] arr2.[0].[0]) (multiplication arr1.[0].[1] arr2.[1].[0])
+    let e01 = sum (multiplication arr1.[0].[0] arr2.[0].[1]) (multiplication arr1.[0].[1] arr2.[1].[1])
+    let e10 = sum (multiplication arr1.[1].[0] arr2.[0].[0]) (multiplication arr1.[1].[1] arr2.[1].[0])
+    let e11 = sum (multiplication arr1.[1].[0] arr2.[0].[1]) (multiplication arr1.[1].[1] arr2.[1].[1])
+    [|[|e00; e01|];[|e10; e11|]|]
+
+
+
+let rec matrixPower arr (n: Num) =
+    if n = toNum1 ()
+    then arr
+    elif mod2 n 
+    then 
+        let mp = matrixPower arr (1, div2 n 0 0 )
+        matrixMultiply mp mp 
+    else 
+        let mp = matrixPower arr (1, div2 (sum n (-1, Cons(1, Empty))) 0 0 )
+        matrixMultiply arr (matrixMultiply mp mp)
 
 let task12FibN n =
-    let toNum1 = (1, Cons(1, Empty))
-    let toNum0 = (1, Cons(0, Empty))
-    
-    let matrixMultiply (arr1 : Num[][]) (arr2 : Num[][]) = 
-        let e00 = sum (multiplication arr1.[0].[0] arr2.[0].[0]) (multiplication arr1.[0].[1] arr2.[1].[0])
-        let e01 = sum (multiplication arr1.[0].[0] arr2.[0].[1]) (multiplication arr1.[0].[1] arr2.[1].[1])
-        let e10 = sum (multiplication arr1.[1].[0] arr2.[0].[0]) (multiplication arr1.[1].[1] arr2.[1].[0])
-        let e11 = sum (multiplication arr1.[1].[0] arr2.[0].[1]) (multiplication arr1.[1].[1] arr2.[1].[1])
-        [|[|e00; e01|];[|e10; e11|]|]
-    
-    let rec matrixPower arr (n: Num) =
-        if n = toNum1
-        then arr
-        elif mod2 n 
-        then 
-            let mp = matrixPower arr (1, div2 n 0 0 )
-            matrixMultiply mp mp 
-        else 
-            let mp = matrixPower arr (1, div2 (sum n (-1, Cons(1, Empty))) 0 0 )
-            matrixMultiply arr (matrixMultiply mp mp)
-     
-    let matrix = [|[|toNum1; toNum1|];[|toNum1; toNum0|]|] 
+    let matrix = [|[|toNum1 (); toNum1 ()|];[|toNum1 (); toNum0 ()|]|] 
     let fibN = matrixPower matrix n
     fibN.[0].[1]
 
 let task13FibN (n : Num) = 
-    let toNum1 = (1, Cons(1, Empty))
     let outArray : Num array = Array.zeroCreate (inInt n)
-    if n = toNum1
-    then outArray.[0] <- toNum1
+    if n = toNum1 ()
+    then outArray.[0] <- toNum1 ()
     else
-        outArray.[0] <- toNum1
-        outArray.[1] <- toNum1
+        outArray.[0] <- toNum1 ()
+        outArray.[1] <- toNum1 ()
         for i in 2..outArray.Length - 1 do 
             outArray.[i] <-  sum outArray.[i - 1] outArray.[i - 2]
     outArray 
@@ -163,11 +159,11 @@ let time f =
 let charting =
     Chart.Combine
         [
-             Chart.Line( [ for i in 1..30 -> (i, time(fun () -> task8FibN (fromInt i) |> ignore) ) ], "Recursion", Color = System.Drawing.Color.Green)
-             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task9FibN (fromInt i) |> ignore) ) ], "Iteration", Color = System.Drawing.Color.Crimson)
-             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task10FibN (fromInt i) |> ignore) ) ], "Iteration without mutable", Color = System.Drawing.Color.DarkGoldenrod)
-             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task11FibN (fromInt i) |> ignore) ) ], "Matrix", Color = System.Drawing.Color.HotPink)
-             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task12FibN (fromInt i) |> ignore) ) ], "Matrix : log", Color = System.Drawing.Color.Maroon)
-             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task13FibN (fromInt i) |> ignore) ) ], "Fib in array", Color = System.Drawing.Color.MediumTurquoise)
+             Chart.Line( [ for i in 1..30 -> (i, time(fun () -> task8FibN (fromInt i) |> ignore) ) ], "Task8", Color = System.Drawing.Color.Green)
+             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task9FibN (fromInt i) |> ignore) ) ], "Task9", Color = System.Drawing.Color.Crimson)
+             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task10FibN (fromInt i) |> ignore) ) ], "Task10", Color = System.Drawing.Color.DarkGoldenrod)
+             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task11FibN (fromInt i) |> ignore) ) ], "Task11", Color = System.Drawing.Color.HotPink)
+             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task12FibN (fromInt i) |> ignore) ) ], "Task12", Color = System.Drawing.Color.Maroon)
+             Chart.Line( [ for i in 1..10..1000 -> (i, time(fun () -> task13FibN (fromInt i) |> ignore) ) ], "Task13", Color = System.Drawing.Color.MediumTurquoise)
         ]
 do System.Windows.Forms.Application.Run(charting.ShowChart())

255e909502b9ba277b5c6709fd9385a340af28f5
MalyshevAnton <daeron.rus@gmail.com>
2015-12-28 05:03:56 +0300

    Done 26 (34)

--- 2015/143/MalyshevAnton/src/Task26/Task26.fs
+++ 2015/143/MalyshevAnton/src/Task26/Task26.fs
@@ -2,8 +2,22 @@
 
 type StrList = List<string * string>
 
+printfn "%s" "Choose method of specifying rules:"
+printfn "%s" "Enter 'console' for setting rules from console"
+printfn "%s" "Enter 'file' for seting rules from file"
+printfn "%s" "Enter 'standart' for setting standart rules (English (lower case) -> ASCII; Example: (a -> 01100001))"
+
+let rulechoice = System.Console.ReadLine()
+
+printfn "%s" "Choose method of entering message:"
+printfn "%s" "Enter 'console' for entering message from console"
+printfn "%s" "Enter 'file' for entering message from file"
+printfn "%s" "Enter 'standart' for standart message: 'Hello world'"
+
+let messagechoice = System.Console.ReadLine()
+
 //Алфавит: {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, x, z, 0, 1, (space)}
-let rules: StrList = [
+let standartRules: StrList = [
     ("a", "01100001")
     ("b", "01100010")
     ("c", "01100011")
@@ -49,35 +63,82 @@ let rec interpretator (rules: StrList) (str: string) =
     | [] -> str
     | hd :: tail ->
         interpretator tail (applyRule hd str)
-    
-//let rec readrules (rules: StrList) =
-//    printfn "%s" "Enter a rule"
-//    let old = System.Console.ReadLine()
-//    printfn "%s" "->"
-//    if old = ""
-//    then
-//        printfn "%s" "Enter a message"
-//        rules
-//    else
-//        let changed = System.Console.ReadLine()
-//        printfn "%s" "To enter a message, enter an empty rule"
-//        let newrules: StrList = [(old, changed)] @ (readrules rules)
-//        newrules
 
-printfn "%s" "Rules: English (lower case) -> ASCII; Example: (a -> 01100001)"
+let rec readConsoleRules (rules: StrList) =
+    printfn "%s" "Enter a rule"
+    let old = System.Console.ReadLine()
+    if old = ""
+    then
+        rules
+    else
+        printfn "%s" "->"
+        let changed = System.Console.ReadLine()
+        printfn "%s" "To enter a message, enter an empty rule"
+        let newrules: StrList = [(old, changed)] @ (readConsoleRules rules)
+        newrules
 
-let rec read (newmessage: string) =
+let rec readConsoleMessages rules (newmessage: string) =
     printfn "%s" "Enter a message, or Enter an empty message to complete"
     let message = System.Console.ReadLine()
     if message <> ""
     then
         printfn "%s" (interpretator rules message)
-        read ""
+        readConsoleMessages rules ""
     else
         printfn "%s" "Done!"
 
+let readFileRules string = 
+    printfn "%s" "Enter the path to the rules" 
+    let path = System.Console.ReadLine() 
+    let filestr = System.IO.File.ReadAllLines(path) 
+    let makeRules (str : string []) = 
+        let mutable rules = [] 
+        let reslength = str.Length 
+        for i in 0..(reslength - 1) do 
+            let istring = str.[i]
+            let istrlen = String.length(istring)
+            let i = istring.IndexOf("->")
+            let old = istring.[0..(i - 1)]
+            let changed = istring.[(i + 2)..(istrlen - 1)]
+            rules <- rules @ [(old, changed)]
+        rules
+    makeRules filestr
+
+let readFileMessages rules =
+    printfn "%s" "Enter the path to the message"
+    let path = System.Console.ReadLine()
+    let strlist = System.IO.File.ReadAllLines(path)
+    printfn "%s" "Enter the path to result message"
+    let pathToNewFile = System.Console.ReadLine()
+    for i in 0..(strlist.Length - 1) do
+        strlist.[i] <- interpretator rules strlist.[i]
+    System.IO.File.AppendAllLines(pathToNewFile, strlist)
+
+let reallymain rulechoice messagechoice =
+
+    let ruler rulechoice =
+        if rulechoice = "console"
+        then readConsoleRules []
+            elif rulechoice = "standart"
+            then standartRules
+                elif rulechoice = "file"
+                then readFileRules ""
+                else standartRules
+    
+    let messager messagechoice =
+        if messagechoice = "console"
+        then readConsoleMessages (ruler rulechoice) ""
+            elif messagechoice = "file"
+            then readFileMessages (ruler rulechoice)
+                elif messagechoice = "standart"
+                then printfn "%s" (interpretator (ruler rulechoice) "Hello world")
+                else printfn "%s" "wrong"
+
+    messager messagechoice
+
+
 [<EntryPoint>]
 let main argv = 
-    read ""
+    reallymain rulechoice messagechoice
     let l = System.Console.ReadLine()
     0
--- 2015/143/MalyshevAnton/tests/Task26.Tests/Task26.Tests.fs
+++ 2015/143/MalyshevAnton/tests/Task26.Tests/Task26.Tests.fs
@@ -4,13 +4,7 @@ open Task26
 open NUnit.Framework
 
 [<Test>]
-let ``hello world`` () =
-    let result = interpretator rules "hello world"
+let ``interpret test`` () =
+    let result = interpretator [("ab", "c"); ("d", "f"); ("c", "g")] "abdabdab"
     printfn "%A" result
-    Assert.AreEqual(result, "0110100001100101011011000110110001101111001000000111011101101111011100100110110001100100")
-
-[<Test>]
-let ``do nothing`` () =
-    let result = interpretator rules "Код"
-    printfn "%A" result
-    Assert.AreEqual(result, "Код")
+    Assert.AreEqual(result, "gfgfg")

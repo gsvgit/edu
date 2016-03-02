2ac02ebee200c3c33295fa97d6e33d4d86621e7b
Polina <lunina_polina@mail.ru>
2016-03-02 13:13:49 +0300

    added module

--- 2015/143/LuninaPolina/src/Task2/Task2.fs
+++ 2015/143/LuninaPolina/src/Task2/Task2.fs
@@ -1,3 +1,5 @@
-﻿let main x = x * x * x * x + x * x * x + x * x + x + 1
+﻿module Task2
+
+let main x = x * x * x * x + x * x * x + x * x + x + 1
 let input = System.Console.ReadLine() |> int
 System.Console.WriteLine(main input)

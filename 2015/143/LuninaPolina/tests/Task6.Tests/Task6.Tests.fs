030b06ae180d9ac91d4d0ce329c41bfd5d93878f
Polina <lunina_polina@mail.ru>
2016-03-02 13:48:59 +0300

    Task6

---
+++ 2015/143/LuninaPolina/tests/Task6.Tests/Task6.Tests.fs
@@ -0,0 +1,18 @@
+﻿module Task6.Tests
+open Task6
+open NUnit.Framework
+
+[<Test>]
+let ``main return [|1;2|]`` () =
+  let res = main [|2;1|]
+  Assert.AreEqual([|1;2|], res)
+
+[<Test>]
+let ``main return [|3;5|]`` () =
+  let res = main [|5;3|]
+  Assert.AreEqual([|3;5|], res)
+
+[<Test>]
+let ``main return [|2;2|]`` () =
+  let res = main [|2;2|]
+  Assert.AreEqual([|2;2|], res)

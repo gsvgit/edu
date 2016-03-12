43ed666c0a51dec5370b54fbb5bfd752f3335c98
Polina <lunina_polina@mail.ru>
2016-03-12 20:11:38 +0300

    пробелы в 18

--- 2015/143/LuninaPolina/src/Task18/Task18.fs
+++ 2015/143/LuninaPolina/src/Task18/Task18.fs
@@ -5,7 +5,7 @@ open Task17
 
 let toMyString (s:string) = 
     let rec f (s1:MyString) i = 
-        if i = s.Length+1
+        if i = s.Length + 1
         then s1
         else f (s1.add(s.[s.Length - i])) (i + 1)
     let (s1:MyString) = Empty

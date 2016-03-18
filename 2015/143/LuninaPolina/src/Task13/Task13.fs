fd7a125db5b457872b6ce64845699a3a71405fe2
Polina <lunina_polina@mail.ru>
2016-03-19 00:56:27 +0300

    13 fixed again

--- 2015/143/LuninaPolina/src/Task13/Task13.fs
+++ 2015/143/LuninaPolina/src/Task13/Task13.fs
@@ -2,7 +2,7 @@
 
 let main n = 
     let outArray = Array.empty
-    for i in 1..n do
+    for i in 0..n do
         if n < 2 
         then outArray.[n] <- n
         else outArray.[n] <- outArray.[n - 1] + outArray.[n - 2]

823592e63ff3133060f88907ebf66dc982a85e60
Polina <lunina_polina@mail.ru>
2016-03-30 22:26:04 +0300

    19

---
+++ 2015/143/LuninaPolina/src/Task19/Task19.fs
@@ -0,0 +1,36 @@
+﻿module Task19
+open Task15
+open Task16
+
+type Tree = 
+    | Leaf of int
+    | Node of int * MyList<Tree>
+
+let compare el (min, max) = 
+            if el < min && el > max
+            then (el,el)
+            elif el < min && el < max 
+            then (el,max)
+            elif el > min && el > max
+            then (min,el)
+            else (min,max)    
+let main (tree:Tree) =    
+    let rec maxmin_tree tree (tmin,tmax) = 
+        match tree with
+        | Leaf(el) -> 
+            compare el (tmin, tmax)
+        | Node(n,list) -> 
+            let rec maxmin_list (list:MyList<Tree>) (lmin,lmax) = 
+                match list with 
+                | Empty -> (lmin,lmax)
+                | Cons(tree,list) -> maxmin_tree tree (maxmin_list list (lmin,lmax))
+            compare n (maxmin_list list (tmin,tmax))          
+    match tree with
+    | Leaf (el) -> maxmin_tree tree (el,el)
+    | Node (n, list) -> maxmin_tree tree (n,n)
+
+
+
+
+
+
